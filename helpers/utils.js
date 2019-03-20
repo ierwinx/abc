e6c995ccfd836bfd487a3c10a2acb7c9624efcb6
Erwin De La Luz De Leon <eluz@bancoazteca.com>
2019-03-20 11:23:37 -0600

    se  agrego handlebars al procesar plantillas email

--- helpers/utils.js
+++ helpers/utils.js
@@ -2,7 +2,7 @@ var logger = require('log4js').getLogger("respuestas");
 var uuidv1 = require('uuid/v1');
 var UsuarioDAO = require('../daos/usuarioDAO');
 var cryptoJs = require('crypto-js');
-var oauth = require("../services/OAUTH/dsi");
+var dsi = require("../services/OAUTH/dsi");
 
 var printJson = function(res, status, mensaje, object) {
     var aleatorio = Math.floor(Math.random() * 99);
@@ -19,22 +19,12 @@ var printJson = function(res, status, mensaje, object) {
     res.status(status).json(json);
 }
 
-var procesaPlantilla = function(html, arreglo) {
-    logger.info("::: Se procesan datos para plantilla email :::");
-    arreglo.forEach((valor, indice, array) => {
-        Object.keys(valor).forEach(key => {
-            html = html.replace('{{' + key + '}}', valor[key]);
-        });
-    });
-    return html;
-}
-
-var verifyToken = async(req, res, next) => {
+var verifyToken = (req, res, next) => {
     logger.info("::: se valida acceso al sistema :::");
     var header = req.headers['authorization'];
     if (!header) {
         logger.error("::: "+process.env.e400+" :::");
-        return printJson(res, 400, process.env.e400, null);
+        return utils.printJson(res, 400, process.env.e400, null);
     }
     var bearer = "";
     try {
@@ -42,26 +32,37 @@ var verifyToken = async(req, res, next) => {
         bearer = bytes.toString(cryptoJs.enc.Utf8);
     } catch(err) {
         logger.error("::: "+process.env.e403+" :::");
-        return printJson(res, 403, process.env.e403, null);
+        return utils.printJson(res, 403, process.env.e403, null);
     }
 
-    var decoded = await oauth.validaToken(bearer).then().catch(err => {
-        return printJson(res, 500, err.message, null);
+    dsi.validaToken(bearer).then(decoded => {
+        dsi.verificaInformacion().then(async(resp) => {
+
+            var usuario = await UsuarioDAO.buscarNumeroUsuario(decoded.user_id).then().catch(err => {
+                return printJson(res, 500, "Favor de registrarse en el sistema", null);
+            });
+
+            if (usuario.usuario != resp.usuario.No_empleado) {
+                printJson(res, 500, "Error al verificar usuario", null);
+            } else {
+                var ip = req.ip.replace(/^([a-z:]+):(\d+).(\d+).(\d+).(\d+)$/g, '$2.$3.$4.$5');
+                if (ip != "::1") {
+                    if (usuario.ip != ip) {
+                        printJson(res, 505, process.env.e505, null);
+                    } else {
+                        next();
+                    }
+                } else {
+                    next();
+                }
+            }
+        }).catch(err => {
+            return printJson(res, 500, err.message, null);
+        })
+    }).catch(err => {
+        return printJson(res, 400, err.message, null);
     });
 
-    var usuario = await UsuarioDAO.buscarNumeroUsuario(decoded.username).then().catch(err => {
-        printJson(res, 500, process.env.e500, null);
-    });
-    var ip = req.ip.replace(/^([a-z:]+):(\d+).(\d+).(\d+).(\d+)$/g, '$2.$3.$4.$5');
-    if (ip != "::1") {
-        if (usuario.ip != ip) {
-            printJson(res, 505, process.env.e505, null);
-        } else {
-            next();
-        }
-    } else {
-        next();
-    }
 }
 
 module.exports = {
